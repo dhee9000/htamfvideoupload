e0c18fcb1447cfa1da79094e8f2b1769b00f44ce
= <=>
2019-01-12 10:25:32 -0500

    Modified for HTAMF Use

--- server.js
+++ server.js
@@ -1,25 +1,28 @@
-var app = require('http').createServer(handler)
-    , io = require('socket.io').listen(app)
+var http = require('http').createServer(handler)
+    , io = require('socket.io').listen(http)
     , fs = require('fs')
     , exec = require('child_process').exec
+    , execSync = require('child_process').execSync
+    , spawn = require('child_process').spawn
     , util = require('util')
+    , ffmpeg = require('fluent-ffmpeg')
+    , express = require('express');
+
+var app = express();
 
 app.listen(8080);
+
+http.listen(69);
 
 console.log("Running on port 8080");
 
 var Files ={};
 
+app.use(express.static('public'));
+app.use("/video", express.static('Video'));
+
 function handler (req, res) {
-    fs.readFile(__dirname + '/index.html',
-        function (err, data) {
-            if (err) {
-                res.writeHead(500);
-                return res.end('Error loading index.html');
-            }
-            res.writeHead(200);
-            res.end(data);
-        });
+
 }
 
 io.sockets.on('connection', function (socket) {
@@ -69,10 +72,39 @@ io.sockets.on('connection', function (socket) {
           var inp = fs.createReadStream("Temp/" + Name);
           var out = fs.createWriteStream("Video/" + Name);
           console.log("Writing " + Name + " to final dir!")
-          inp.pipe(out)
-          fs.unlink("Temp/" + Name, () => {});
-          console.log("All operations on " + Name + " finished!")
-          socket.emit('Done', {});
+          var op = inp.pipe(out)
+          op.on('finish', () => {
+            fs.unlink("Temp/" + Name, () => {
+              console.log("Combining Intro and Outro on " + Name);
+
+              var cmd = 'ffmpeg';
+              var filename = Name.split(".")[0];
+
+              cmdFull = 'ffmpeg -i Source/Intro.mp4 -i Video/' + filename + ' -i Source/Outro.mp4 -f lavfi -i color=black -filter_complex "[1:v]scale=w=1920:h=1080:force_original_aspect_ratio=2[scaled]; [scaled]crop=1920:1080[cropped]; [cropped]fps=30[fpsset]; [0:v]fade=t=out:st=6:d=1:alpha=1, setpts=PTS-STARTPTS[introfade]; [introfade][0:a][fpsset][1:a][2:v][2:a]concat=n=3:v=1:a=1[outv][outa]" -map "[outv]" -map "[outa]" Video/' + filename + '_Final.mp4';
+
+              fs.writeFileSync("convert.bat", cmdFull);
+
+              var args = [
+                '-i', 'Source/Intro.mp4',
+                '-i', 'Video/' + filename,
+                '-i', 'Source/Outro.mp4',
+                '-f', 'lavfi',
+                '-i', 'color=black',
+                '-filter_complex', '"[1:v]scale=w=1920:h=1080:force_original_aspect_ratio=2[scaled]; [scaled]crop=1920:1080[cropped]; [cropped]fps=30[fpsset]; [0:v]fade=t=out:st=6:d=1:alpha=1, setpts=PTS-STARTPTS[introfade]; [introfade][0:a][fpsset][1:a][2:v][2:a]concat=n=3:v=1:a=1[outv][outa]"',
+                '-map', '[outv]',
+                '-map', '[outa]',
+                '-f', 'mp4', 'Video/' + filename + '_Final.mp4'
+              ];
+
+              console.log("FFMPEG Starting Convert and Combine");
+
+              var proc = exec("convert.bat")
+              proc.on('exit', ()=> {
+                socket.emit('Done', {'URL': 'video/' + Name.split(".")[0] + "_Final.mp4"});
+                console.log("All operations on " + Name + " finished!");
+              });
+            });
+          });
         });
     }
     else if(Files[Name]['Data'].length > 10485760){ //If the Data Buffer reaches 10MB
